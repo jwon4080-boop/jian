0c8700a2dd7662f1038d8a332d8a9f086e547432
jwon4080-boop <jwon4080@gmail.com>
2026-07-20 11:13:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ import streamlit as st
 st.markdown("# 앱UI만들기")
 user_id = st.text_input("이름", placeholder="이름")
 ai_model = st.radio("학년", ["1", "2", "3"], horizontal=True)
-user_id = st.text_input("반", value="1")
-
+cls = st.number_input("반", value="1")
+user_id = st.text_input("아이디(ID)를 입력하세요", placeholder="example_user")
+age = st.number_input("나이를 입력하세요", min_value=1, max_value=100, value=17)
