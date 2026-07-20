4c6a179c06a87a323fcbee0df4f261532e27e1c0
jwon4080-boop <jwon4080@gmail.com>
2026-07-20 14:28:36 +0900

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
 def reset_all():
-  st.seossion_state.user_name = ""
-  st.seossion_state.weather = "맑음"
-  st.seossion_state.top_type = "후드티"
-  st.seossion_state.top_color = "밝음"
-  st.seossion_state.bottom_type = "청바지"
-  st.seossion_state.bottom_color = "슬림"
-  st.seossion_state.shoes = "스니커즈"
-  st.seossion_state.acc = []
+  st.session_state.user_name = ""
+  st.session_state.weather = "맑음"
+  st.session_state.top_type = "후드티"
+  st.session_state.top_color = "밝음"
+  st.session_state.bottom_type = "청바지"
+  st.session_state.bottom_color = "슬림"
+  st.session_state.shoes = "스니커즈"
+  st.session_state.acc = []
     
 with st.sidebar:
   st.header("프로필")
