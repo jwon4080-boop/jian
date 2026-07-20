a5780f6bef850e86a8c4df0e7e94323833eae6be
jwon4080-boop <jwon4080@gmail.com>
2026-07-20 12:55:12 +0900

    Update abc.py

--- abc.py
+++ abc.py
@@ -14,7 +14,7 @@ col1, col2 = st.columns(2)
 with col1:
    st.subheader("상의")
    top_type = st.radio("종류", ["후드티","셔츠","맨투맨","반팔 티셔츠"])
-  top_color = st.select_slider("색상 톤", options=["밝음","무난함","어두움"])
+   top_color = st.select_slider("색상 톤", options=["밝음","무난함","어두움"])
 with co12:
   st.subheader("하의")
   bottom_type = st.radio("종류",["청바지","슬랙스","트레이닝 팬츠","반바지"])
