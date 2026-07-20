58f791768b0a225976444c6e7cd7b1411cf6f363
jwon4080-boop <jwon4080@gmail.com>
2026-07-20 12:20:03 +0900

    Update abc.py

--- abc.py
+++ abc.py
@@ -9,7 +9,7 @@ with st.sidebar:
 
 st.title("👗AI 코디 메이커")
 st.write("사이드바에서 날씨를 먼저 선택하고 시작하세요!")
-st.header("아이템 조합하기")
+st.header("👕아이템 조합하기")
 col1, col2 = st.columns(2)
 with col1:
 st.subheader("상의")
