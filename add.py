2f4c2331cc245e18fc95f1048ab26361a9cb19b8
jwon4080-boop <jwon4080@gmail.com>
2026-07-20 13:40:39 +0900

    Create add.py

---
+++ add.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("카운터 앱")
+if 'count' not in st.sesson_state:
+    st.session_state.count = 0
+if st.button("증가"):
+    st.session_state.count += 1
+  st.markdown(f"##현재 숫자: '{st.session_state.count}'")
