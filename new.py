e2c72569d96c9af6c93dd4561d0c153b52c48812
jwon4080-boop <jwon4080@gmail.com>
2026-07-21 12:09:00 +0900

    Update new.py

--- new.py
+++ new.py
@@ -1,7 +1,8 @@
 import streamlit as st
-
 from openai import OpenAI
+
 ai_client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
 st.title("🌍 AI 환경 오염도 체크 프로그램")
 st.write("환경 오염 물질 수치를 입력하면 AI가 위험도를 측정합니다.")
 
@@ -10,4 +11,3 @@ pm10 = st.number_input(
     min_value=0.0,
     value=30.0
 )
-                       
