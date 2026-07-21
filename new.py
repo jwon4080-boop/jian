6338cb3e489aecdfd6807d903fe644b63c245777
jwon4080-boop <jwon4080@gmail.com>
2026-07-21 13:02:36 +0900

    Update new.py

--- new.py
+++ new.py
@@ -12,7 +12,28 @@ pm10 = st.number_input(
     value=30.0
 )
 pm25 = st.number_input(
-    "초미세먼지 pm2.5 (㎍/㎥)",
+    "초미세먼지 PM2.5 (㎍/㎥)",
     min_value=0.0,
     value=15.0
+)
+co2 = st.number_input(
+    "이산화탄소 CO2 (ppm)",
+    min_value=300,
+    value=600
+)
+co = st.number_input(
+    "일산화탄소 CO (ppm)",
+    min_value=0.0,
+    value=1.0
+)
+voc = st.number_input(
+    "휘발성 유기 화합물 VOC (ppb)"
+    min_value=0.0,
+    value=100.0
+)
+water = st.number_input(
+      "수질 오염도 ",
+       min_value=0,
+       max_value=100,
+       value=20
 )
